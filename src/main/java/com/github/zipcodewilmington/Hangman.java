2840ad78b5c89836864e6f56e0434bb44115a86f
annafu1 <104175541+annafu1@users.noreply.github.com>
2022-12-03 15:31:23 -0500

    followed directions

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -2,6 +2,7 @@ package com.github.zipcodewilmington;
 
 //anna
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -11,49 +12,99 @@ import java.util.Scanner;
  * @date 5/27/21 11:02 AM
  */
 public class Hangman {
+
+    public Hangman () {
+
+    }
+    private static String[] phrase = {"Pear", "Dear", "Farm", "Corn"};
     Scanner scanner = new Scanner(System.in);
-    Random rand = new Random();
-    int phraseRandom = rand.nextInt(2);
-    int numOfGuesses = 0;
-    char[] randomWord;
+    Random randomWord = new Random();
+    int randomWordGenerator = randomWord.nextInt(4);
+    int guesses = 0;
+    char[] word;
     char[] arr;
-    String[] phrase = {"sugar", "paper", "mills", "farms"}
-    public static void main(String[] args) {
-        Hangman letPlay = new Hangman();
-        letPlay.runGame();
+
+    public void main(String[] args) {
+        Hangman game = new Hangman();
+        game.runGame();
     }
 
     public void runGame() {
-        intializeGameStat();
         announceGame();
+        initializeGameState();
+        while (!isWordGuessed()) {
+            printCurrentStatus();
+            process(getNextGuess());
+            guesses++;
+            if (guesses == 3) {
+                System.exit(0);
+            }
+            winner();
+            askToPlayAgain();
+        }
+        loser();
+        gameOver();
+        askToPlayAgain();
+    }
+
+    public void printArray(char[] charArr) {
+        for (int i = 0; i < charArr.length ; i++) {
+            System.out.println(charArr[i] + " ");
+        }
     }
 
     public void announceGame() {
-        System.out.println("let's play hangman!");
-    }
-    public void intializeGameStat() {
-        arr = new char[] {'_', '_', '_','_'};
-        randomWord = phrase[phraseRandom].toCharArray();
+        System.out.println("Welcome to the game of hangman!");
     }
 
-    public Character nextGuess() {
+    public void gameOver() {
+        System.out.println("Game over");
+    }
+
+    public void initializeGameState() {
+        arr = new char[]{'_', '_', '_', '_'};
+    }
+
+    public Character getNextGuess() {
         System.out.println("Guess a letter: ");
         return scanner.next().charAt(0);
     }
 
-
-
-
-    public void endGame() {
-        System.out.println("Game over");
+    public boolean isWordGuessed() {
+        return Arrays.equals(word, arr);
     }
 
-    public void playerWon() {
-        System.out.println("You are a winner!");
+    public boolean askToPlayAgain() {
+        System.out.println("Would you like to play again?\n 1. y or 2.n");
+        int playInput = scanner.nextInt();
+        if (playInput == 1) {
+            runGame();
+        }
+        System.exit(0);
+        return true;
     }
 
-    public void playerLost() {
-        System.out.println("You lose");
+    public void printCurrentStatus() {
+        this.printArray(arr);
     }
 
+    public void process(Character guess) {
+        for (int i = 0; i < word.length; i++) {
+            if (word[i] == guess) {
+                arr[i] = word[i];
+            }
+        }
+    }
+
+//    public void randWordToArray() {
+//        word = phrase[randomWordGenerator].toCharArray();
+//    }
+
+    public void winner() {
+        System.out.println("You won!");
+    }
+
+    public void loser() {
+        System.out.println("Sorry, try again!");
+    }
 }
